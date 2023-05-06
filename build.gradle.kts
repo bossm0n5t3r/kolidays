5e70669ff4baeac26ea8bdea195dfb946dfae645
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-05-06 23:21:21 +0900

    :octocat:

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("jvm") version "1.8.20"
+    application
+
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
+}
+
+group = "me.bossm0n5t3r"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+ktlint {
+    version.set("0.48.2")
+}
