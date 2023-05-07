a4c3ddd3c3127d184b16a5d981976e91e93cbce3
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-05-07 09:05:50 +0900

    feat: Add batch/build.gradle.kts

---
+++ batch/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "1.8.20"
+
+    application
+}
+
+group = "me.bossm0n5t3r.kolidays"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.github.kittinunf.fuel:fuel:3.0.0-alpha1")
+
+    testImplementation(kotlin("test"))
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
