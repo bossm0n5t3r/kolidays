6e5d795d2dce715ef229d544030dba7bf8f2e711
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-05-10 23:00:00 +0900

    feat: Add BatchHttpClient and edit Constants

--- batch/src/main/kotlin/me/bossm0n5t3r/kolidays/batch/Constants.kt
+++ batch/src/main/kotlin/me/bossm0n5t3r/kolidays/batch/Constants.kt
@@ -4,4 +4,16 @@ object Constants {
     object DefaultValue {
         const val SECRET_KEY = "SERVICE_KEY"
     }
+
+    object HttpMethod {
+        const val GET = "GET"
+    }
+
+    object HttpHeaders {
+        const val ACCEPT = "Accept"
+    }
+
+    object MediaType {
+        const val APPLICATION_JSON_VALUE = "application/json"
+    }
 }
