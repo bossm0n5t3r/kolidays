b3a85f1563611b6cda66416787bcefd6c1144667
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-05-13 06:00:30 +0900

    fix: Edit Utils
    - Add String.fromYYYYMMDDToLocalDate
    - Add LocalDate.toConstructorString

--- batch/src/main/kotlin/me/bossm0n5t3r/kolidays/batch/Utils.kt
+++ batch/src/main/kotlin/me/bossm0n5t3r/kolidays/batch/Utils.kt
@@ -4,10 +4,18 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 object Utils {
     val objectMapper: ObjectMapper = jacksonObjectMapper()
         .registerKotlinModule()
         .enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT)
         .enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+
+    fun String.fromYYYYMMDDToLocalDate(): LocalDate {
+        return LocalDate.parse(this, DateTimeFormatter.ofPattern("yyyyMMdd"))
+    }
+
+    fun LocalDate.toConstructorString() = "LocalDate.of(${this.year}, ${this.monthValue}, ${this.dayOfMonth})"
 }
