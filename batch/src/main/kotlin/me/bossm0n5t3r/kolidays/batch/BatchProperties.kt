46707155347a587612ad8ad810c174966f517176
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-05-13 06:05:40 +0900

    fix: Edit BatchProperties
    - Use PROJECT_DIR_ABSOLUTE_PATH

--- batch/src/main/kotlin/me/bossm0n5t3r/kolidays/batch/BatchProperties.kt
+++ batch/src/main/kotlin/me/bossm0n5t3r/kolidays/batch/BatchProperties.kt
@@ -1,12 +1,12 @@
 package me.bossm0n5t3r.kolidays.batch
 
+import me.bossm0n5t3r.kolidays.batch.Constants.PROJECT_DIR_ABSOLUTE_PATH
 import java.io.FileInputStream
 import java.nio.file.Paths
 import java.util.Properties
 
 object BatchProperties {
-    private val projectDirAbsolutePath = Paths.get("").toAbsolutePath().toString()
-    private val resourcesPath = Paths.get(projectDirAbsolutePath, "/batch/src/main/resources/application.conf")
+    private val resourcesPath = Paths.get(PROJECT_DIR_ABSOLUTE_PATH, "/batch/src/main/resources/application.conf")
     private val prop = Properties().apply {
         load(FileInputStream(resourcesPath.toFile()))
     }
